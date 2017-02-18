fc3b54936747f2c9667a313d1a1267c1ea31b154
u1 <u1.kobayashi@gmail.com>
2017-02-18 19:34:00 +0900

    Implement step4 of “Yasashi Dagger2”.

---
+++ app/src/beta/java/com/u1fukui/android/demo/dagger/yasashi/Cat.java
@@ -0,0 +1,9 @@
+package com.u1fukui.android.demo.dagger.yasashi;
+
+public class Cat implements Pet {
+
+    @Override
+    public String getName() {
+        return "たま";
+    }
+}
---
+++ app/src/beta/java/com/u1fukui/android/demo/dagger/yasashi/SampleModule.java
@@ -0,0 +1,13 @@
+package com.u1fukui.android.demo.dagger.yasashi;
+
+import dagger.Module;
+import dagger.Provides;
+
+@Module
+public class SampleModule {
+
+    @Provides
+    Pet providePet() {
+        return new Cat();
+    }
+}
